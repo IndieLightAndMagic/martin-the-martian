ab4ed512436de0f11bfa0ac2edc2eb2de95cbca9
Julian Guarin <julian.guarin@globant.com>
2018-05-04 15:06:59 -0500

    Keyboard event registering.

--- src/Event/eventkeyboard_sdlbridge.cpp
+++ src/Event/eventkeyboard_sdlbridge.cpp
@@ -1,136 +1,164 @@
 #include "eventkeyboard_sdlbridge.h"
 
-namespace GTech {
+#include <map>
+#include <vector>
+#include <functional>
 
+using namespace std;
+namespace GTech {
+	namespace KeyboardEventDispatcher{
+
+		vector<function<void()>> vSDLKBEventDispatch;
+		using vectorSignalsKBEventKey = vector<Signal<GTech::KBEvent&, GTech::KBKey&>&>;
+		using vectorLambda = vector<function<void()>>;
+		map<Uint64, vectorLambda> mDispatchRegisteredLambda;
+		map<Uint64, vectorSignalsKBEventKey> mDispatchRegisteredSignal;
+		vector<Uint64> vRegisteredKBEvents;
+
+
+	}
 	namespace KeyboardEvent{
         constexpr Uint32 SDLKBEvent(GTech::KBEvent& rKBEvent){
             switch(rKBEvent){
                 case GTech::KBEvent::KEY_PRESSED:
                     return SDL_KEYDOWN;
-                    break;
+
                 case GTech::KBEvent::KEY_RELEASED:
                     return SDL_KEYUP;
-                    break;
+
                 default:
                     return 0;
             }
         };
-        constexpr GTech::KBEvent KBEvent(Uint32& rSDLKBEvent){
+        constexpr GTech::KBEvent KBEvent_SDL(Uint32& rSDLKBEvent){
             switch(rSDLKBEvent){
                 case SDL_KEYDOWN:
                     return GTech::KBEvent::KEY_PRESSED;
-                    break;
+
                 case SDL_KEYUP:
                     return GTech::KBEvent::KEY_RELEASED;
-                    break;
+
                 default:
                     return GTech::KBEvent::KEY_UNKNOWNEVENT;
             }
         };
     }
     namespace KeyboardKey{
-    	constexpr GTech::KBKey KBKey(Sint32& rSDLKBKey){
+    	constexpr GTech::KBKey KBKey_SDL(Sint32& rSDLKBKey){
     		switch(rSDLKBKey){
     			case SDLK_a:        
                 	return GTech::KBKey::K_A;
-                	break;
+
                 case SDLK_d:        
                 	return GTech::KBKey::K_D;
-                	break;
+
                 case SDLK_w:        
                 	return GTech::KBKey::K_W;
-                	break;
+
                 case SDLK_s:        
                 	return GTech::KBKey::K_S;
-                	break;
+
                 
                 case SDLK_UP:       
                 	return GTech::KBKey::K_UP;
-                	break;
+
                 case SDLK_DOWN:     
                 	return GTech::KBKey::K_DOWN;
-                	break;
+
                 case SDLK_LEFT:     
                 	return GTech::KBKey::K_LEFT;
-                	break;
+
                 case SDLK_RIGHT:    
                 	return GTech::KBKey::K_RIGHT;
-                	break;
-                
+
                 case SDLK_ESCAPE:   
                 	return GTech::KBKey::K_ESC;
-                	break;
+
                 case SDLK_SPACE:    
                 	return GTech::KBKey::K_SPACE;
-                	break;
+
                 case SDLK_UNKNOWN:
                 default:
                 	return GTech::KBKey::K_UNKNOWN;
-                	break;
+
     		}
 		};
 		constexpr Sint32 SDLKBKey(GTech::KBKey & rKBKey){
     		switch(rKBKey){
+
     			case GTech::KBKey::K_A :        
                 	return SDLK_a;
-                	break;
+
                 case GTech::KBKey::K_D :        
                 	return SDLK_d;
-                	break;
+
                 case GTech::KBKey::K_W :        
                 	return SDLK_w;
-                	break;
+
                 case GTech::KBKey::K_S :        
                 	return SDLK_s;
-                	break;
+
                 case GTech::KBKey::K_UP :       
                 	return SDLK_UP;
-                	break;
+
                 case GTech::KBKey::K_DOWN :     
                 	return SDLK_DOWN;
-                	break;
+
                 case GTech::KBKey::K_LEFT :     
                 	return SDLK_LEFT;
-                	break;
+
                 case GTech::KBKey::K_RIGHT :    
                 	return SDLK_RIGHT;
-                	break;
+
                 case GTech::KBKey::K_ESC :   
                 	return SDLK_ESCAPE;
-                	break;
+
                 case GTech::KBKey::K_SPACE :    
                 	return SDLK_SPACE;
-                	break;
+
                 case GTech::KBKey::K_UNKNOWN :
                 	return SDLK_UNKNOWN;
-                	break;
+
                 default:
 					return SDLK_UNKNOWN;
-                	break;
     		}
 		};
         
     }
 
-	void RegisterKeyboardEvent(KBEvent& rEvent, KBKey& rKeyButton, Signal<KBEvent&, KBKey&>& rSignal){
+	void RegisterKeyboardEvent_SDL(KBEvent& rEvent, KBKey& rKeyButton, Signal<KBEvent&, KBKey&>& rSignal){
 
-	    //SDL_assert(KeyboardKey::GTechSDLMap.find(rKeyButton) != KeyboardKey::GTechSDLMap.end());
-	    //SDL_assert(KeyboardEvent::GTechSDLMap.find(rEvent) != KeyboardEvent::GTechSDLMap.end());
-		auto dispatcher = [=](){
-			rSignal.emit(rEvent, rKeyButton);
+		auto sdlEvent64	= static_cast<Uint64>(KeyboardEvent::SDLKBEvent(rEvent)) << 32;
+		auto sdlKey64 	= static_cast<Uint64>(KeyboardKey::SDLKBKey(rKeyButton));
+		auto mask64		= sdlEvent64 | sdlKey64;
+
+		auto rvSignals	= KeyboardEventDispatcher::mDispatchRegisteredSignal[mask64];
+		rvSignals.push_back(rSignal);
+
+
+		auto l = [=](){
+			//rSignal.emit(rEvent, rKeyButton);
+			rSignal.disconnect_all();
 		};
-		std::vector<decltype(dispatcher)>dispatcherVector;
-
-		
+		auto vRegisteredLambda = KeyboardEventDispatcher::mDispatchRegisteredLambda[mask64];
+		if (vRegisteredLambda.empty()){
+			KeyboardEventDispatcher::vRegisteredKBEvents.push_back(mask64);
+		}
+		vRegisteredLambda.push_back(l);
 
 
 	}
 
-	void DispatchKeyboardEvent(Uint32 sdlevent, Sint32 sdlkeycode){
+	void DispatchKeyboardEvent_SDL(Uint32& rKBEvent,  Sint32& rKBKey){
 
-		using namespace KeyboardEventDispatch;
-	    for (auto& signal:keyboardEventDispatch[sdlevent][sdlkeycode]){
-	        signal.emit(KeyboardEvent::SDLGTechMap[sdlevent], KeyboardKey::SDLGTechMap[sdlkeycode]);
-	    }
+		auto mask64 = (static_cast<Uint64>(rKBEvent) << 32) | static_cast<Uint64>(rKBEvent);
+		for (const auto& registeredKBEvent: KeyboardEventDispatcher::vRegisteredKBEvents){
+
+			if (mask64 != registeredKBEvent) continue;
+			auto vLambdas	= KeyboardEventDispatcher::mDispatchRegisteredLambda[mask64];
+			for (const auto& lambda: vLambdas)
+				lambda();
+
+		}
 	}	
 }
--- src/Event/eventkeyboard_sdlbridge.h
+++ src/Event/eventkeyboard_sdlbridge.h
@@ -1,7 +1,6 @@
 #ifndef __EVENTKEYBOARD_SDL_BRIDGE_H__
 #define __EVENTKEYBOARD_SDL_BRIDGE_H__
 
-#include <map>
 
 #include "Tech.h"
 #include "Tech_SDLBridge.h"
@@ -19,18 +18,18 @@ namespace GTech{
     namespace KeyboardEvent{
 
         constexpr Uint32 SDLKBEvent(GTech::KBEvent& rKBEvent);
-        constexpr GTech::KBEvent KBEvent(Uint32& rSDLKBEvent);
+        constexpr GTech::KBEvent KBEvent_SDL(Uint32& rSDLKBEvent);
 
     }
     namespace KeyboardKey{
-        constexpr GTech::KBKey KBKey(Sint32& rSDLKBKey);
+        constexpr GTech::KBKey KBKey_SDL(Sint32& rSDLKBKey);
         constexpr Sint32 SDLKBKey(GTech::KBKey & rKBKey);
     }
     /*!
     \brief Register a generic KBEvent, for a generic keybutton, with a generic Signal.
     */
-    void RegisterKeyboardEvent(KBEvent& rEvent, KBKey& rKeyButton, Signal<KBEvent&, KBKey&>& rSignal);
-    void DispatchKeyboardEvent(Uint32 sdlevent, Sint32 sdlkeycode);
+    void RegisterKeyboardEvent_SDL(KBEvent& rEvent, KBKey& rKeyButton, Signal<KBEvent&, KBKey&>& rSignal);
+    void DispatchKeyboardEvent_SDL(Uint32& rKBEvent, Sint32& rKBKey);
 
 
 
